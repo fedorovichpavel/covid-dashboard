2f19c04e6278f014078283df50c448d9ea4ca4f2
Maryna Migacheva <m.migacheva@gmail.com>
2020-12-19 12:19:15 +0300

    feat: add flags, update modal and style

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -13,20 +13,21 @@ function initEventsDOM() {
       if(event.target.tagName === 'I') {
         target = event.target.parentElement;
       };
-      const listHiddenModule = document.querySelectorAll('.wrap-data > div');
+      // const listHiddenModule = document.querySelectorAll('.wrap-data > div');
+      const listHiddenModule = document.querySelectorAll('.wrap-data .widget');
       if(!target.classList.contains('close')) {
         //hidden elements
         listHiddenModule.forEach((item) => {
           item.classList.toggle('window-hidden');
         });
         //element active full width
-        target.parentElement.parentElement.classList.toggle('full-width');
+        target.parentElement.classList.toggle('full-width');
         target.innerHTML = '<i class="material-icons">close</i>';
         target.classList.toggle('close');
       } else {
         //close the window that is full width
         target.classList.toggle('close');
-        target.parentElement.parentElement.classList.toggle('full-width');
+        target.parentElement.classList.toggle('full-width');
         target.innerHTML = '<i class="material-icons">zoom_out_map</i>';
         listHiddenModule.forEach((item) => {
           item.classList.toggle('window-hidden');
@@ -35,5 +36,5 @@ function initEventsDOM() {
     })
   });
 
-  
+
 }
